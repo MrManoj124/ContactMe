01c9345dc14c29b1f5049552409e72602d0e5fb8
MrManoj124 <manojmanorooban@gmail.com>
2025-12-16 17:02:36 +0530

    fetch contacts for AminDashboard function

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -2,28 +2,28 @@ import { useState } from 'react';
 import React, {useState, useEffect} from 'react'
 import {Mail, Eye, Check, Trash2, RefreshCw} from 'lucide-react';
 
-// const AdminDashboard = () => {
-//     cosnt [contacts, setContacts] = useState([]);
-//     const [analytics, setAnalytics] = useState(null);
-//     const [loading, setLoading] = useState(true);
-//     const [activeTab, setActiveTab] = useState('contacts');
-//     const [filter, setFilter] = useState('all');
+const AdminDashboard = () => {
+    cosnt [contacts, setContacts] = useState([]);
+    const [analytics, setAnalytics] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [activeTab, setActiveTab] = useState('contacts');
+    const [filter, setFilter] = useState('all');
 
-//     const API_URL = 'http://localhost:5000/api';
+    const API_URL = 'http://localhost:5000/api';
 
-//   useEffect(() => {
-//     fetchData();
-//   }, [filter]);
+  useEffect(() => {
+    fetchData();
+  }, [filter]);
 
-//   const fetchData = async () => {
-//     setLoading(true);
-//     try {
-//         // Fetch contacts
-//       const statusParam = filter !== 'all' ? `?status=${filter}` : '';
-//       const contactsRes = await fetch(`${API_URL}/contacts${statusParam}`);
-//       const contactsData = await contactsRes.json();
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+        // Fetch contacts
+      const statusParam = filter !== 'all' ? `?status=${filter}` : '';
+      const contactsRes = await fetch(`${API_URL}/contacts${statusParam}`);
+      const contactsData = await contactsRes.json();
       
-//       if (contactsData.success) {
-//         setContacts(contactsData.data);
-//       }
-// }
+      if (contactsData.success) {
+        setContacts(contactsData.data);
+      }
+}
