086f1f23cdfaaf0b68771faa5828e1b2503998da
MrManoj124 <manojmanorooban@gmail.com>
2025-12-17 23:57:13 +0530

    created a Footer function for Footer.js file

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -2,3 +2,11 @@ import React from 'react';
 import { Github, Linkedin, Twitter, Facebook, Instagram, Mail, Heart } from 'lucide-react';
 import { profileData } from '../../data/profile';
 
+const Footer = () => {
+  const socialLinks = [
+    { icon: Github, url: profileData.social.github, label: 'GitHub' },
+    { icon: Linkedin, url: profileData.social.linkedin, label: 'LinkedIn' },
+    { icon: Twitter, url: profileData.social.twitter, label: 'Twitter' },
+    { icon: Facebook, url: profileData.social.facebook, label: 'Facebook' },
+    { icon: Instagram, url: profileData.social.instagram, label: 'Instagram' },
+  ];
