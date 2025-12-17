7deb01b94587aaf6932472b7a22dd36f4033729a
MrManoj124 <manojmanorooban@gmail.com>
2025-12-18 00:00:52 +0530

    created a About function inside the About.js file

---
+++ frontend/src/components/sections/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { profileData } from '../../data/profile';
+
+const About = () => {
+  const stats = [
+    { number: "20+", label: "Projects Completed" },
+    { number: "15+", label: "Technologies" },
+    { number: "500+", label: "GitHub Commits" },
+    { number: "100%", label: "Client Satisfaction" }
+  ];
