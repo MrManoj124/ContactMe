31b63d26e5ee5a99c451053f7a9e000a1c213612
MrManoj124 <manojmanorooban@gmail.com>
2025-12-26 22:32:07 +0530

    Start to list Alll the Connection

--- backend/testDb.js
+++ backend/testDb.js
@@ -1,1 +1,20 @@
 //testDb.js - Test MongoDb Connection
+const mongoose=require("mongoose");
+require ('dotenv').config();
+
+const testConnnection= async () => {
+    try{
+        console.log(' 🔄 sAttempting to Connecting to MongoDB....');
+        console.log('Connection String : ',process.env.MONGODB_URI || 'mongodb://localhost:27017/portfolio');
+
+        await mongodb.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/portfolio',{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        });
+        console.log('MongoDB Connected Successfully!');
+        console.log('Database name : ',mongoose.connection.db.portfolio);
+
+        //List All collection
+        
+    }
+}
