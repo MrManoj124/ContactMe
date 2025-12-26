60b81da1c03e2adab917581470ff95ef8d18c4b4
MrManoj124 <manojmanorooban@gmail.com>
2025-12-27 01:19:40 +0530

    Start code for index.html file

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name= "theme-color" content="#22c55e"/>
+    <meta />
+</head>
+<body>
+    
+</body>
+</html>
