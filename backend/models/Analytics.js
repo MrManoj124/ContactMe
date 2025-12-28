b7a40f509152ff94b4d2cbb90413088776415e10
MrManoj124 <manojmanorooban@gmail.com>
2025-12-28 23:37:57 +0530

    create indexes for Analytics.js file

--- backend/models/Analytics.js
+++ backend/models/Analytics.js
@@ -1,1 +1,22 @@
 const mongoose=require('mongoose');
+const { applyTimestamps } = require('./Contact');
+
+const analyticsSchema = new mongoose.Schema({
+    eventType:{
+        type:String,
+        required:true,
+        enum:['page_view','project_view', 'contact_submit','resume_download','social_click'],   
+    },
+    eventData:{
+        type:mongoose.Schema.Types.Mixed
+    },
+    sessionInfo:{
+        ipAddress:String,
+        userAgent:String
+    },
+    timestamp:{
+        type:Date,
+        default:Date.now
+    },
+});
+
