74ee2386dce615aad80b7aa99d6456998b4a334e
MrManoj124 <manojmanorooban@gmail.com>
2025-12-28 23:55:30 +0530

    Complete the code for models/ProjectView.js file

--- backend/models/ProjectView.js
+++ backend/models/ProjectView.js
@@ -1,3 +1,25 @@
 //ProjectView.js file
 const mongoose = require('mongoose');
 
+const projectViewSchema=new mongoose.Schema({
+    projectId:{
+        type:String,
+        required:true
+    },
+    projectTitle:{
+        type:String,
+        required:true,
+    },
+    viewCount:{
+        type:Number,
+        default:1,
+    },
+    lastViewed:{
+        type:Date,
+        default:Date.now
+    },
+});
+
+projectViewSchema.index({projectId:1});
+ 
+module.exports = mongoose.model('ProjectView', projectViewSchema);
