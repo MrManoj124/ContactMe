2b2e2e78c47b5770d61f5a56a282be035fcb1fe1
MrManoj124 <manojmanorooban@gmail.com>
2025-12-30 01:33:01 +0530

    start to code submit contact form

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -5,3 +5,21 @@ const Analytics = require('../models/Analytics');
 const rateLimit = require('../middleware/rateLimit');
 const {getRequestMetaData} = require('../utils/helpers');
 
+//Submit contact form
+// router.post('/',rateLimit(3, 60000), async(req,res) => {
+//     try{
+//         const {name, email, subject, message} = req.body;
+
+//         if(!name || !email || !subject ||!message){
+//             return res.status(400).json({
+//                 success:false,
+//                 message:'All fields are required'
+//             });
+//         }
+
+//         const contact = new Contact({
+//             name, email, subject, message, metadata: getRequestMetaData(req)
+//         });
+//     await contact.save();
+    }
+})
