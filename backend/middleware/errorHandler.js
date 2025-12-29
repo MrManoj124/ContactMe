bb31477a42f78c537baaf471b3c100b06983c2d1
MrManoj124 <manojmanorooban@gmail.com>
2025-12-29 23:21:02 +0530

    complete the code for errorHandler.js file

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,3 +1,10 @@
 const errorHandler = (error, req, res, next) => {
-    
-}
+    console.error('Error:',error);
+
+    res.status(error.status || 500).json({
+        success:false,
+        message : error.message || 'Internal server error'
+    });
+};
+
+module.exports = errorHandler;
