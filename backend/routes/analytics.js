c322d6480670905af5da3c9d343cab47d4fb810c
MrManoj124 <manojmanorooban@gmail.com>
2025-12-30 23:57:13 +0530

    get all the require files for analytics.js file

--- backend/routes/analytics.js
+++ backend/routes/analytics.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Analytics = require('../models/Analytics');
+const Contact = require('../models/Contact');
+const {getRequestMetadata} = require('../utils/helpers');
+
