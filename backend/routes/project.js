606acac084066b10c87a22deb361c45bb512700e
MrManoj124 <manojmanorooban@gmail.com>
2026-01-03 01:53:38 +0530

    Track the Project View

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -3,3 +3,31 @@ const router = express.Router();
 const ProjectView = require('../models/ProjectView');
 
 
+//Track project View
+router.post('/view', async (req,res) => {
+    try{
+        const {projectId, projectTitle} = req.body;
+
+        await ProjectView.findOneAndUpdate(
+            {projectId},
+            {
+                projectId,
+                projectTitle,
+            $inc:{viewCount : 1},
+        lastViewed : Date.now()
+              },
+              {upsert : true, new:true}
+        );
+
+        re.json({
+            success:true,
+            message : 'Project view tracked'
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:'Failed to track view'
+        });
+    }
+})
