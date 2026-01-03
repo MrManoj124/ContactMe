7c75b41acba3665be399fa68f59d798b510f66ef
MrManoj124 <manojmanorooban@gmail.com>
2026-01-03 22:55:26 +0530

    code for src/utils/constatnts.js file

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,13 @@
+export const PROFILE_DATA = {
+  name: "S.Manoj",
+  title: "Full Stack Developer",
+  email: "manojmanorooban@gmail.com",
+  phone: "+94 760543464",
+  location: "Colombo, Sri Lanka"
+};
+
+export const SOCIAL_LINKS = [
+  { platform: "GitHub", url: "https://github.com/yourusername" },
+  { platform: "LinkedIn", url: "https://linkedin.com/in/yourusername" },
+  { platform: "Twitter", url: "https://twitter.com/yourusername" }
+];
