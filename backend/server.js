a4f4a0dcffd422704d23564aa074578d610cc969
MrManoj124 <manojmanorooban@gmail.com>
2026-01-03 23:01:12 +0530

    import all Routes file into server.js file

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,8 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 const errorHandler = require('./middleware/errorHandler');
 
+// Import Routes
+const contactRoutes = require('./routes/contact');
+const analyticsRoutes = require('./routes/analytics');
+const projectRoutes = require('./routes/project');
+
