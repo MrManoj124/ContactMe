ca4e19a2b0dc1c891606152378b05dc6d3788dbe
MrManoj124 <manojmanorooban@gmail.com>
2026-02-04 21:37:03 +0530

    update import files

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 import {Mail, Eye, Check, Trash2, RefreshCw} from 'lucide-react';
 
 const AdminDashboard = () => {
