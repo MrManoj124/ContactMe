2c6f3e5b016bbff7ad51d65ba1b213ce7de7d95a
MrManoj124 <manojmanorooban@gmail.com>
2026-02-05 13:15:04 +0530

    start to create DeveloperPortfolio function

--- frontend/src/components/Portfolio.jsx
+++ frontend/src/components/Portfolio.jsx
@@ -1,3 +1,5 @@
 //Let's start
 import React, {useState, useEffect} from 'react';
 import { Menu, X, Github, Linkedin, Facebook, Instagram, Twitter, Mail, Code, Database, Server, Palette, Cpu, Brain, ArrowRight, ExternalLink, Download } from 'lucide-react';}
+
+const DeveloperPortfolio = () => {
