78e9654410da9958db53ed2ff8eedb72f80cbb7a
MrManoj124 <manojmanorooban@gmail.com>
2026-02-06 21:07:26 +0530

    update database.js file

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 
-const connectDB=async() => {
-    try{
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser : true,
-            useUnifiedTopology : true,
-        });
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
 
         console.log('MongoDB Connected Successfully');
-        console.log(`Database : ${mongoose.connection.db.portfolio}  `);    
-    }catch(error){
-        console.error('MongoDB Connection Error : ',error.message);
+        console.log(`Database : ${mongoose.connection.db.portfolio}  `);
+    } catch (error) {
+        console.error('MongoDB Connection Error : ', error.message);
         process.exit(1);
     }
 };
